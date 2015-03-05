28826f82536e6afca1799fe07c584bc23554f6d3
Giovanni Ortuño <gd.ortuno@gmail.com>
2015-03-05 15:46:37 -0800

    Toasts displaying reason why the app is being closed

--- app/src/main/java/io/github/webbluetoothcg/bletestperipheral/Peripherals.java
+++ app/src/main/java/io/github/webbluetoothcg/bletestperipheral/Peripherals.java
@@ -18,6 +18,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.UUID;
@@ -125,7 +126,7 @@ public class Peripherals extends Activity {
     mBluetoothAdapter = mBluetoothManager.getAdapter();
     // Check if bluetooth is supported
     if (mBluetoothAdapter == null) {
-      //TODO(g-ortuno): Show message in the UI and close the app
+      Toast.makeText(this, R.string.bluetoothNotSupported, Toast.LENGTH_LONG).show();
       Log.e(TAG, "Bluetooth not supported");
       finish();
     } else if (!mBluetoothAdapter.isEnabled()) {
@@ -134,7 +135,7 @@ public class Peripherals extends Activity {
       startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
     } else if (!mBluetoothAdapter.isMultipleAdvertisementSupported()) {
       // Make sure device supports LE advertising
-      //TODO(g-ortuno): Show message in the UI and close the app
+      Toast.makeText(this, R.string.bluetoothAdvertisingNotSupported, Toast.LENGTH_LONG).show();
       Log.e(TAG, "Advertising not supported");
       finish();
     } else {
@@ -149,7 +150,7 @@ public class Peripherals extends Activity {
     if (requestCode == REQUEST_ENABLE_BT) {
       if (resultCode == RESULT_OK) {
         if (!mBluetoothAdapter.isMultipleAdvertisementSupported()) {
-          //TODO(g-ortuno): Show message in the UI
+          Toast.makeText(this, R.string.bluetoothAdvertisingNotSupported, Toast.LENGTH_LONG).show();
           Log.e(TAG, "Advertising not supported");
           finish();
         } else {
@@ -157,6 +158,7 @@ public class Peripherals extends Activity {
         }
       } else {
         //TODO(g-ortuno): UX for asking the user to activate bt
+        Toast.makeText(this, R.string.bluetoothNotEnabled, Toast.LENGTH_LONG).show();
         Log.e(TAG, "Bluetooth not enabled");
         finish();
       }
