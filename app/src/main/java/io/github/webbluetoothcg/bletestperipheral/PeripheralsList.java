32e969addf2af6bb94d00ebe38f60aa896de7183
Giovanni Ortuño Urquidi <gd.ortuno@gmail.com>
2015-03-17 14:00:30 -0700

    Use ArrayAdapter instead of custom adapter

--- app/src/main/java/io/github/webbluetoothcg/bletestperipheral/PeripheralsList.java
+++ app/src/main/java/io/github/webbluetoothcg/bletestperipheral/PeripheralsList.java
@@ -1,15 +1,11 @@
 package io.github.webbluetoothcg.bletestperipheral;
 
 import android.app.ListActivity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 
 public class PeripheralsList extends ListActivity {
@@ -21,7 +17,10 @@ public class PeripheralsList extends ListActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_peripherals_list);
-    PeripheralsListAdapter adapter = new PeripheralsListAdapter(this, PERIPHERALS_NAMES);
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+        /* layout for the list item */ android.R.layout.simple_list_item_1,
+        /* id of the TextView to use */ android.R.id.text1,
+        /* values for the list */ PERIPHERALS_NAMES);
     setListAdapter(adapter);
   }
 
@@ -34,59 +33,4 @@ public class PeripheralsList extends ListActivity {
     startActivity(intent);
   }
 
-  private class PeripheralsListAdapter extends BaseAdapter{
-
-    private LayoutInflater mLayoutInflater;
-    private String[] mPeripheralsNames;
-
-    public PeripheralsListAdapter(Context context, String[] peripheralsNames) {
-      mLayoutInflater = LayoutInflater.from(context);
-      mPeripheralsNames = peripheralsNames;
-    }
-
-    @Override
-    public int getCount() {
-      return mPeripheralsNames.length;
-    }
-
-    @Override
-    public String getItem(int position) {
-      return mPeripheralsNames[position];
-    }
-
-    @Override
-    public long getItemId(int position) {
-      return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-      View view;
-      // We create a view holder to save a reference to the views in the row. That way we don't
-      // have to call findViewById() every time.
-      ViewHolder holder;
-      // If convertView is null then the row is being added for the first time and we
-      // need to inflate its view and save references to its children views.
-      if (convertView == null) {
-        view = mLayoutInflater.inflate(R.layout.list_item_peripheral, parent,
-         // Layout will be inflated but won't be attached to any other layout so it won't be drawn,
-         // receive touch events, etc.
-         /* attach to root */ false);
-        holder = new ViewHolder();
-        holder.textViewPeripheralName = (TextView) view.findViewById(R.id.textView_peripheralName);
-        // Set tag to store data within a view without resorting to another data structure.
-        view.setTag(holder);
-      } else {
-        view = convertView;
-        holder = (ViewHolder) view.getTag();
-      }
-      holder.textViewPeripheralName.setText(getItem(position));
-
-      return view;
-    }
-
-    private class ViewHolder {
-      public TextView textViewPeripheralName;
-    }
-  }
 }
