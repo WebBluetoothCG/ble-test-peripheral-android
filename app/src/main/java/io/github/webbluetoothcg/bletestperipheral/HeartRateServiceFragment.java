4c7c7705c91c66383a6b5759fe8f6ee360bf02e8
Giovanni Ortuño Urquidi <gd.ortuno@gmail.com>
2015-03-18 12:53:12 -0700

    Fixes based on comments

--- app/src/main/java/io/github/webbluetoothcg/bletestperipheral/HeartRateServiceFragment.java
+++ app/src/main/java/io/github/webbluetoothcg/bletestperipheral/HeartRateServiceFragment.java
@@ -1,6 +1,5 @@
 package io.github.webbluetoothcg.bletestperipheral;
 
-
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattService;
 import android.os.Bundle;
@@ -12,12 +11,16 @@ import java.util.UUID;
 
 public class HeartRateServiceFragment extends ServiceFragment {
 
-  ///////////////////////
-  ////// Constants //////
-  ///////////////////////
+  /**
+   * See <a href="https://developer.bluetooth.org/gatt/services/Pages/ServiceViewer.aspx?u=org.bluetooth.service.heart_rate.xml">
+   * Heart Rate Service</a>
+   */
   private static final UUID HEART_RATE_SERVICE_UUID = UUID
       .fromString("0000180D-0000-1000-8000-00805f9b34fb");
-
+  /**
+   * See <a href="https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.body_sensor_location.xml">
+   * Body Sensor Location</a>
+   */
   private static final UUID BODY_SENSOR_LOCATION_UUID = UUID
       .fromString("00002A37-0000-1000-8000-00805f9b34fb");
 
